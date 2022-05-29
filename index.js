f88632c4c0e5c9ea6985eca67540a7065288249b
WDsagor <sagarwpress@gmail.com>
2022-05-29 23:16:39 +0600

    admin route add

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
@@ -13,27 +13,27 @@ app.get("/", (req, res) => {
   res.send("port a kiu paowa jaitese");
 });
 
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v1nj2ca.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 const verifyToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
-  if(!authHeader){
-    return res.status(401).send({message: 'Unauthorized access'});
+  if (!authHeader) {
+    return res.status(401).send({ message: "Unauthorized access" });
   }
-  const token = authHeader.split(' ')[1];
-  jwt.verify(token, process.env.ACCESS_TOKEN_SEC, function(err, decoded) {
-    if(err){
-      return res.status(403).send({message: 'Forbidden access'});
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SEC, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
     }
     req.decoded = decoded;
     next();
   });
-   
-}
+};
 
 async function run() {
   try {
@@ -41,7 +41,7 @@ async function run() {
     const productCollection = client.db("cycle_ghor").collection("products");
     const orderCollection = client.db("cycle_ghor").collection("orders");
     const userCollection = client.db("cycle_ghor").collection("users");
-    
+
     app.get("/", (req, res) => {
       res.send("port a kiu paowa jaitese");
     });
@@ -64,29 +64,69 @@ async function run() {
       const result = await productCollection.findOne(query);
       res.send(result);
     });
+    app.get("/myorders/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const order = await orderCollection.find(query).toArray();
+      res.send(order);
+    });
+    app.get("/users", verifyToken, async (req, res) => {
+      const query = {};
+      const allUser = await userCollection.find(query).toArray();
+      res.send(allUser);
+    });
+    app.get('/admin/:email', verifyToken, async(req, res) =>{
+      const email =req.params.email;
+      const user = await userCollection.findOne({email: email});
+      const isAdmin = user.role === 'admin';
+      res.send({admin: isAdmin})
+    })
     //post API---------
     app.post("/products/:id", async (req, res) => {
       const product = req.body;
       const result = await orderCollection.insertOne(product);
-      console.log(result);
+      
       res.send({
         success: true,
         message: `Successfully Ordered ${product.itemName}!`,
       });
     });
-
-    app.put("/user/:email", async( req, res)=>{
-      const email =req.params.email;
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
       const user = req.body;
-      const filter = {email: email};
-      const options = {upsert: true};
+      const filter = { email: email };
+      const options = { upsert: true };
       const updateUser = {
         $set: user,
+      };
+      const result = await userCollection.updateOne(
+        filter,
+        updateUser,
+        options
+      );
+      const accessToken = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SEC,
+        { expiresIn: "3h" }
+      );
+      res.send({ result, accessToken });
+    });
+
+    app.put("/user/admin/:email",verifyToken, async (req, res) => {
+      const email = req.params.email;
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({email:requester});
+      if(requesterAccount.role === 'admin'){
+
+        const filter = { email: email };
+        const updateUser = {
+          $set: { role: "admin" },
+        };
+        const result = await userCollection.updateOne(filter, updateUser);
+        console.log(result);
+        res.send(result);
       }
-      const result = await userCollection.updateOne(filter, updateUser, options);
-      const accessToken = jwt.sign({email:email}, process.env.ACCESS_TOKEN_SEC,{ expiresIn: '3h' })
-      res.send({result, accessToken});
-    })
+    });
 
     app.delete("/orders/:id", async (req, res) => {
       const id = req.params.id;
@@ -99,7 +139,6 @@ async function run() {
 
       res.send({ success: true, message: "Successfully deleted " });
     });
-    
   } finally {
   }
 }
